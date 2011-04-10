286db483c9b352d8dc13a31d90aa9725bc433bed
forthebreeze@gmail.com <forthebreeze@gmail.com@3a9e36a6-82f4-eae3-afe6-3fe4f2d7c8d7>
2011-04-10 05:50:50 +0000

    CHI selection

---
+++ trunk/src/core/preprocess/featureSelection/CHIFeatrueSelection.java
@@ -0,0 +1,25 @@
+package core.preprocess.featureSelection;
+
+import core.preprocess.util.DataAnalyzer;
+
+public class CHIFeatrueSelection extends FeatureSlection{
+
+	@Override
+	public String[] featureSelection(DataAnalyzer data, String[] featureSet) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean selection(DataAnalyzer data, int featureId, double thresh) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean selection(DataAnalyzer data, String featureId, double thresh) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
