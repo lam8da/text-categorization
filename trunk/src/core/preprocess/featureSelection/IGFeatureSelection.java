d848a046f577493f0b84b5f01c10e85d195253cb
forthebreeze@gmail.com <forthebreeze@gmail.com@3a9e36a6-82f4-eae3-afe6-3fe4f2d7c8d7>
2011-04-10 05:51:37 +0000

    Information Gain Selection

---
+++ trunk/src/core/preprocess/featureSelection/IGFeatureSelection.java
@@ -0,0 +1,24 @@
+package core.preprocess.featureSelection;
+
+import core.preprocess.util.DataAnalyzer;
+
+public class IGFeatureSelection extends FeatureSlection {
+
+	@Override
+	public String[] featureSelection(DataAnalyzer data, String[] featureSet) {
+		return null;
+	}
+
+	@Override
+	public boolean selection(DataAnalyzer data, int featureId, double thresh) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean selection(DataAnalyzer data, String featureId, double thresh) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
