a5715ced8b23834b076e7a06921a839fc5a36c0b
forthebreeze@gmail.com <forthebreeze@gmail.com@3a9e36a6-82f4-eae3-afe6-3fe4f2d7c8d7>
2011-04-11 08:50:27 +0000

    Modify some interface and implement the methods

--- trunk/src/core/preprocess/selection/CHIFeatrueSelection.java
+++ trunk/src/core/preprocess/selection/CHIFeatrueSelection.java
@@ -1,25 +1,70 @@
 package core.preprocess.selection;
 
 import core.preprocess.util.DataAnalyzer;
+import java.util.ArrayList;
 
 public  class CHIFeatrueSelection extends FeatureSelection{
 
 	@Override
-	public boolean selection(DataAnalyzer data, int featureId, double thresh) {
+	public String[] maxFeatureSelection(DataAnalyzer data, String[] featureSet, double thresh) {
 		// TODO Auto-generated method stub
-		return false;
+		double max;
+		ArrayList<String> result = new ArrayList<String>();
+		for(int i = 0; i != featureSet.length; i++){
+			max = this.getMaxSelectionWeighting(data, data.getFeatureId(featureSet[i]));
+			if(max > thresh){
+				result.add(featureSet[i]);
+			}
+		}
+		return (String[])result.toArray();
+	}
+	
+	@Override
+	public String[] avgFeatureSelection(DataAnalyzer data, String[] featureSet, double thresh) {
+		// TODO Auto-generated method stub
+		double avg;
+		ArrayList<String> result = new ArrayList<String>();
+		for(int i = 0; i != featureSet.length; i++){
+			avg = this.getAvgSelectionWeighting(data, data.getFeatureId(featureSet[i]));
+			if(avg > thresh){
+				result.add(featureSet[i]);
+			}
+		}
+		return (String[])result.toArray();
 	}
 
 	@Override
-	public boolean selection(DataAnalyzer data, String featureId, double thresh) {
+	public double getAvgSelectionWeighting(DataAnalyzer data, int featureId) {
 		// TODO Auto-generated method stub
-		return false;
+		double A,B,C,D,L;
+		double avg = 0;
+		for(int i = 0; i != data.getM(); i++){
+			A = data.getN_ci_tk(i, featureId);
+			B = data.getN_not_ci_tk(i, featureId);
+			C = data.getN_ci_exclude_tk(i, featureId);
+			D = data.getN_not_ci_exclude_tk(i, featureId);
+			L = data.getN_ci(i);
+			avg += L*(A*D-C*B)*(A*D-C*B)/((A+C)*(B+D)*(A+B)*(C+D));
+		}
+		return avg;
 	}
-
 	@Override
-	public String[] featureSelection(DataAnalyzer data, String[] featureSet, double thresh) {
+	public double getMaxSelectionWeighting(DataAnalyzer data, int featureId) {
 		// TODO Auto-generated method stub
-		return null;
+		double A,B,C,D,L;
+		double max = 0;
+		double tmp;
+		for(int i = 0; i != data.getM(); i++){
+			A = data.getN_ci_tk(i, featureId);
+			B = data.getN_not_ci_tk(i, featureId);
+			C = data.getN_ci_exclude_tk(i, featureId);
+			D = data.getN_not_ci_exclude_tk(i, featureId);
+			L = data.getN_ci(i);
+			tmp = L*(A*D-C*B)*(A*D-C*B)/((A+C)*(B+D)*(A+B)*(C+D));
+			if(tmp > max){
+				max = tmp;
+			}
+		}
+		return max;
 	}
-
 }
--- trunk/src/core/preprocess/selection/MIFeatureSelection.java
+++ trunk/src/core/preprocess/selection/MIFeatureSelection.java
@@ -1,26 +1,73 @@
 package core.preprocess.selection;
 
 import core.preprocess.util.DataAnalyzer;
-
+import java.lang.Math;
+import java.util.ArrayList;
 public class MIFeatureSelection extends FeatureSelection{
 
 	@Override
-	public boolean selection(DataAnalyzer data, int featureId, double thresh) {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
-	@Override
-	public boolean selection(DataAnalyzer data, String featureId, double thresh) {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
-	@Override
-	public String[] featureSelection(DataAnalyzer data, String[] featureSet,
+	public String[] avgFeatureSelection(DataAnalyzer data, String[] featureSet,
 			double thresh) {
 		// TODO Auto-generated method stub
-		return null;
+		double tmp;
+		ArrayList<String> result = new ArrayList<String>();
+		for(int i = 0; i != featureSet.length; i++){
+			tmp = this.getAvgSelectionWeighting(data, data.getFeatureId(featureSet[i]));
+			if(tmp > thresh){
+				result.add(featureSet[i]);
+			}
+		}
+		return (String[])result.toArray();
 	}
+
+	@Override
+	public double getAvgSelectionWeighting(DataAnalyzer data, int featureId) {
+		// TODO Auto-generated method stub
+		double res = 0;
+		double A,B,C,L;
+		for(int i = 0; i != data.getM(); i++){
+			L = data.getN_ci(i);
+			A = data.getN_ci_tk(i, featureId);
+			B = data.getN_not_ci_tk(i, featureId);
+			C = data.getN_ci_exclude_tk(i, featureId);
+			res += (A*L/(A+C)/(A+B));
+		}
+		return res;
+	}
+
+	@Override
+	public double getMaxSelectionWeighting(DataAnalyzer data, int featureId) {
+		// TODO Auto-generated method stub
+		double res = 0;
+		double tmp;
+		double A,B,C,L;
+		for(int i = 0; i != data.getM(); i++){
+			L = data.getN_ci(i);
+			A = data.getN_ci_tk(i, featureId);
+			B = data.getN_not_ci_tk(i, featureId);
+			C = data.getN_ci_exclude_tk(i, featureId);
+			tmp = (A*L/(A+C)/(A+B));
+			if(tmp > res){
+				res = tmp;
+			}
+		}
+		return res;
+	}
+
+	@Override
+	public String[] maxFeatureSelection(DataAnalyzer data, String[] featureSet,
+			double thresh) {
+		// TODO Auto-generated method stub
+		double tmp;
+		ArrayList<String> result = new ArrayList<String>();
+		for(int i = 0; i != featureSet.length; i++){
+			tmp = this.getMaxSelectionWeighting(data, data.getFeatureId(featureSet[i]));
+			if(tmp > thresh){
+				result.add(featureSet[i]);
+			}
+		}
+		return (String[])result.toArray();
+	}
+
 
 }
