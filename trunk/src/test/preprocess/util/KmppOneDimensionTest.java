ab08ef8cbd7939b1d852c8cc39d71333c387a434
forthebreeze@gmail.com <forthebreeze@gmail.com@3a9e36a6-82f4-eae3-afe6-3fe4f2d7c8d7>
2011-05-13 05:13:06 +0000

    Modify interfaces of feature selection

---
+++ trunk/src/test/preprocess/util/KmppOneDimensionTest.java
@@ -0,0 +1,22 @@
+package test.preprocess.util;
+
+import java.util.Random;
+
+import core.preprocess.util.KmppOneDimension;
+public class KmppOneDimensionTest {
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		double[] data = new double[10];
+		Random r = new Random();
+		for(int i = 0; i != 10; i++){
+			data[i] = r.nextDouble();
+		}
+		KmppOneDimension kmpp = new KmppOneDimension(data,3,100);
+		System.out.println("number of interactions: "+kmpp.Cluster());
+		System.out.println("Threshold: "+kmpp.GetThresh());
+		kmpp.Output();
+	}
+}
