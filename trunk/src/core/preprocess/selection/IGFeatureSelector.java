7b98ee49a73020f7caf1ff9c4c2a86b1d45678a3
forthebreeze@gmail.com <forthebreeze@gmail.com@3a9e36a6-82f4-eae3-afe6-3fe4f2d7c8d7>
2011-05-26 12:48:06 +0000

    fix nan bug

--- trunk/src/core/preprocess/selection/IGFeatureSelector.java
+++ trunk/src/core/preprocess/selection/IGFeatureSelector.java
@@ -15,17 +15,21 @@ public class IGFeatureSelector extends FeatureSelector {
 		// TODO Auto-generated method stub
 		double N, L, T, T_, A, C;
 		double res, tmp1 = 0, tmp2 = 0, tmp3 = 0;
+		double log2 = Math.log(2);
 		N = analyzer.getN();
-		T_ = analyzer.getN_exclude_tk(featureId);
-		T = analyzer.getN_tk(featureId);
-		
-		for (int i = 0; i != analyzer.getM(); i++) {
-			L = analyzer.getN_ci(i);
-			tmp1 -= (L / N) * Math.log(L / N);
-			A = analyzer.getN_ci_tk(i, featureId);
-			tmp2 += (A / T) * Math.log(A / T);
-			C = analyzer.getN_ci_exclude_tk(i, featureId);
-			tmp3 += (C / T_) * Math.log(C / T_);
+		double logN = Math.log(N);
+		T_ = analyzer.getN_exclude_tk(featureId)+1;
+		double logT_ = Math.log(T_);
+		T = analyzer.getN_tk(featureId)+1;
+		double logT = Math.log(T);
+		int M = analyzer.getM();
+		for (int i = 0; i != M; i++) {
+			/*L = analyzer.getN_ci(i);
+			tmp1 -= (L / N) * (Math.log(L)/log2-logN/log2);*/
+			A = analyzer.getN_ci_tk(i, featureId)+1;
+			tmp2 += (A / T) * (Math.log(A) /log2-logT/log2);
+			C = analyzer.getN_ci_exclude_tk(i, featureId)+1;
+			tmp3 += (C / T_) * (Math.log(C) /log2-logT_/log2);
 		}
 		tmp2 = T / N * tmp2;
 		tmp3 = T_ / N * tmp3;
