70971baf99e85c1ac4fa34f153bc722a2f74e4f0
forthebreeze@gmail.com <forthebreeze@gmail.com@3a9e36a6-82f4-eae3-afe6-3fe4f2d7c8d7>
2011-05-26 14:56:44 +0000

    apply add-one-like data smoothing

--- trunk/src/core/preprocess/selection/MIFeatureSelector.java
+++ trunk/src/core/preprocess/selection/MIFeatureSelector.java
@@ -13,15 +13,16 @@ public class MIFeatureSelector extends FeatureSelector {
 	public double getAvgSelectionWeighting(int featureId) {
 		// TODO Auto-generated method stub
 		double res = 0;
-		double A, B, C,N,L;
+		double A, B, C,N,L,M;
 		double log2 = Math.log(2);
 		N = analyzer.getN();
-		for (int i = 0; i != analyzer.getM(); i++) {
+		M = analyzer.getM();
+		for (int i = 0; i != M; i++) {
 			L = analyzer.getN_ci(i);
 			A = analyzer.getN_ci_tk(i, featureId);
 			B = analyzer.getN_not_ci_tk(i, featureId);
 			C = analyzer.getN_ci_exclude_tk(i, featureId);
-			res += L/N*Math.log( (A * N / (A + C) / (A + B)) )/log2;
+			res += L/N*Math.log( (A * N+1) / ((A + C)*(A + B)+1) )/log2;
 		}
 		return res;
 	}
@@ -31,13 +32,14 @@ public class MIFeatureSelector extends FeatureSelector {
 		// TODO Auto-generated method stub
 		double res = 0;
 		double tmp;
-		double A, B, C, N;
+		double A, B, C, N,M;
 		N = analyzer.getN();
-		for (int i = 0; i != analyzer.getM(); i++) {
+		M = analyzer.getM();
+		for (int i = 0; i != M; i++) {
 			A = analyzer.getN_ci_tk(i, featureId);
 			B = analyzer.getN_not_ci_tk(i, featureId);
 			C = analyzer.getN_ci_exclude_tk(i, featureId);
-			tmp = (A *N / (A + C) / (A + B));
+			tmp = (A *N+1) / ((A + C)*(A + B)+1);
 			if (tmp > res) {
 				res = tmp;
 			}
